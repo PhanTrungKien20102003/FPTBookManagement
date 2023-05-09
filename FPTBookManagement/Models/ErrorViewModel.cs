d28d3885a10cb9b477e287cef908b2896787f512
PhanTrungKien20102003 <newjourney20102003@gmail.com>
2023-05-09 09:32:58 +0700

    Add project files.

---
+++ FPTBookManagement/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace FPTBookManagement.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
